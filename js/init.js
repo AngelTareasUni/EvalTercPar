56a20c753abaa64192b7ffa1d2459290afe578e1
AngelTareasUni <87628929+AngelTareasUni@users.noreply.github.com>
2021-08-02 20:38:30 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyARBJQijQgrCsiZhkNw9zuFkoK9IgltHmA",
-    authDomain: "evalterparsaaa.firebaseapp.com",
-    projectId: "evalterparsaaa",
-    storageBucket: "evalterparsaaa.appspot.com",
-    messagingSenderId: "344723191032",
-    appId: "1:344723191032:web:feb6dff4d207a8a2db770d",
-    measurementId: "G-39XZQ9Q0V3"
+    apiKey: "AIzaSyDCnrMPU4i_rNskjqDSbcvRa-L-uGmupvc",
+    authDomain: "evaltercparcsaaa.firebaseapp.com",
+    projectId: "evaltercparcsaaa",
+    storageBucket: "evaltercparcsaaa.appspot.com",
+    messagingSenderId: "1065755627465",
+    appId: "1:1065755627465:web:f9df8d33c54bf93e07e218",
+    measurementId: "G-P5CEXZZQCV"
 });
